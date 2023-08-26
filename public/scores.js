73b8c8aba843b70f77b20f713bc88e10c375a527
alonbarnathan3 <138671708+alonbarnathan3@users.noreply.github.com>
2023-08-26 10:02:22 +0300

    Update scores.js

--- public/scores.js
+++ public/scores.js
@@ -3,7 +3,7 @@ const APP_URL = 'http://localhost:3000';
 
 async function list() {
   // const response = await fetch('https://web-workshop-gules.vercel.app', {method: 'GET'})
-  const response = await fetch(`${APP_URL}/results`, {method: 'GET'})
+  const response = await fetch(`/results`, {method: 'GET'})
   return await response.json()
 }
 
@@ -41,6 +41,10 @@ document.addEventListener('DOMContentLoaded', async () => {
  const intermediate = scores.sort((item1, item2) => item2.intermediateScore - item1.intermediateScore).slice(0,5).map(item => ({userName: item.userName, intermediateScore: item.intermediateScore}));
  const expert = scores.sort((item1, item2) => item2.expertScore - item1.expertScore).slice(0,5).map(item => ({userName: item.userName, expertScore: item.expertScore}));
 
+ const beginnersAverage = calculateAverage(beginners, 'beginnerScore');
+ const intermediateAverage = calculateAverage(intermediate, 'intermediateScore');
+ const expertAverage = calculateAverage(expert, 'expertScore');
+
  console.log("response scores", scores);
  console.log("begginers", beginners);
  console.log("intermediate", intermediate);
@@ -51,4 +55,21 @@ document.addEventListener('DOMContentLoaded', async () => {
 populateTable(beginners, 'beginnersTable');
 populateTable(intermediate, 'intermediateTable');
 populateTable(expert, 'expertTable');
+
+  // Display average scores
+  displayAverage(beginnersAverage, 'beginnerAverage');
+  displayAverage(intermediateAverage, 'intermediateAverage');
+  displayAverage(expertAverage, 'expertAverage');
+
 });
+
+function calculateAverage(data, scoreProperty) {
+  const filteredData = data.filter(item => item[scoreProperty] > 0);
+  const totalScore = filteredData.reduce((sum, item) => sum + item[scoreProperty], 0);
+  return filteredData.length > 0 ? totalScore / filteredData.length : 0;
+}
+
+function displayAverage(average, averageId) {
+  const averageCell = document.getElementById(averageId);
+  averageCell.textContent = average.toFixed(2); // Display average with 2 decimal places
+}
